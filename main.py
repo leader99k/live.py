45d2f4489ffb0226b5b7d6dc668292992c93aedb
leader99k <166257451+leader99k@users.noreply.github.com>
2024-05-18 04:10:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -103,7 +103,7 @@ def main(message):
 					bot.reply_to(message, msg)
 				else:
 					dd += 1
-				time.sleep(20)
+				time.sleep(15)
 	except Exception as e:
 		print(e)
 	bot.edit_message_text(chat_id=message.chat.id, message_id=ko, text='𝗕𝗘𝗘𝗡 𝗖𝗢𝗠𝗣𝗟𝗘𝗧𝗘𝗗 ✅\n𝗕𝗢𝗧 𝗕𝗬 ➜ @NOTBASRA')
